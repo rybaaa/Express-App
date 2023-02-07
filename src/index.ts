e7073af63e2868405281954d774ab0fda181f015
rybaaa <bolshayaryba22@gmail.com>
2023-02-07 14:08:22 +0300

    add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import {usersRouter} from "./routes/usersRouter";
+import cors from 'cors'
 
 const app = express()
 const port = process.env.PORT || 5000
 const parserMiddleware = bodyParser({})
+
+app.use(cors({
+    origin: '*'
+}))
 app.use(parserMiddleware)
 
 app.use('/users', usersRouter)
